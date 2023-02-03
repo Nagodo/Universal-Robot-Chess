a9dcf01d093e75d93d5d00ffdc82bb07d1a66406
Nagodo <89977444+Nagodo@users.noreply.github.com>
2023-02-03 23:08:20 +0100

    Brikker

--- board.py
+++ board.py
@@ -1,37 +1,41 @@
 #Chess Board class
 boardSetup = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
+
 class Board:
     def __init__(self):
-        self.board = [[0 for i in range(8)] for j in range(8)]
+        self.board = [[[] for i in range(8)] for j in range(8)]
 
         self.LoadFEN(boardSetup)
        
 
     def LoadFEN(self, fen):
         #Loop through each character in the FEN string
+        x = 0
+        y = 0
         for i in range(len(fen)):
             #Hvis tegnet er et mellemrum så stopper vi
             if fen[i] == " ":
                 break
 
-            #Hvis tegnet er et tal skal vi placere det antal tomme felter
-            elif fen[i].isdigit():
+            if fen[i] == "/":
+                y += 1
+                x = 0
+                continue
+
+            #Hvis tegnet er et tal så skal vi springe det antal pladser
+            if fen[i] in "12345678":
                 for j in range(int(fen[i])):
-                    self.board[i][j] = 0
+                    self.board[y][x] = [0, ""]
+                    x += 1
+                continue
 
-            # Hvis tegnet er en bogstav skal vi placere en brik
-            elif fen[i].isalpha():
-                self.board[i][j] = fen[i]
-
-            #Hvis tegnet er en skråstreg skal vi gå en række ned
-            elif fen[i] == "/":
-                i += 1
-                j = 0
-            #If the character is anything else, then we know we have an invalid FEN string
-            else:
-                print("Invalid FEN string")
-                break
-
+            #Hvis tegnet er en bogstav så skal vi sætte det på brættet
+            if fen[i] in "rnbqkp":
+                self.board[y][x] = [0, fen[i]]
+                x += 1
+            elif fen[i] in "RNBQKP":
+                self.board[y][x] = [1, fen[i].lower()]
+                x += 1
 
     
--- game.py
+++ game.py
@@ -1,4 +1,72 @@
 import pygame
+from board import Board
+
+#Pygame
+pygame.init()
+pygame.display.set_caption("Skak")
+screen = pygame.display.set_mode((1200, 900))
+clock = pygame.time.Clock()
 
 #Chess Game
 
+
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.DrawBoard(screen)
+
+    def DrawBoard(self, screen):
+        screen.fill((0, 0, 0))
+        for i in range(8):
+            for j in range(8):
+                if (i + j) % 2 == 0:
+                    pygame.draw.rect(screen, (195,160,101), (i * 100, j * 100, 100, 100))
+                else:
+                    pygame.draw.rect(screen, (131,89,53), (i * 100, j * 100, 100, 100))
+
+        #Tegn brikkerne med deres farve ved brug af spritesheet
+        for i in range(8):
+            for j in range(8):
+                if self.board.board[i][j] != [0, ""]:
+                    s = ""
+                    if self.board.board[i][j][0] == 0:
+                        s = "w-"
+                    else:
+                        s = "b-"
+
+                    if self.board.board[i][j][1] == "p":
+                        s += "pawn"
+                    elif self.board.board[i][j][1] == "r":
+                        s += "rook"
+                    elif self.board.board[i][j][1] == "n":
+                        s += "knight"
+                    elif self.board.board[i][j][1] == "b":
+                        s += "bishop"
+                    elif self.board.board[i][j][1] == "q":
+                        s += "queen"
+                    elif self.board.board[i][j][1] == "k":
+                        s += "king"
+
+                    sprite = pygame.image.load("./sprites/" + s + ".png")
+                    #Scale the sprite to 100x100
+                    sprite = pygame.transform.scale(sprite, (100, 100))
+                    screen.blit(sprite, (j * 100, i * 100))
+
+                  
+
+
+#Game Loop
+def main():
+    game = Game()
+    running = True
+    while running:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.flip()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
