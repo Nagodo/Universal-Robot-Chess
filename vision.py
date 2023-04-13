9de10aaae8988b7b411b39af19f90f6f425f15b8
Nagodo <89977444+Nagodo@users.noreply.github.com>
2023-04-13 11:55:46 +0200

    vison

--- vision.py
+++ vision.py
@@ -1,5 +1,8 @@
 import cv2
 from threading import Thread
+from skimage.metrics import structural_similarity
+import pyautogui
+import numpy as np
 import time
 import random
 from CONFIG import *
@@ -12,77 +15,97 @@ class Vision:
         self.newframe = None
 
     def set_chessboard_coords(self):
-        gray = cv2.cvtColor(self.oldframe, cv2.COLOR_BGR2GRAY)
+        # gray = cv2.cvtColor(self.oldframe, cv2.COLOR_BGR2GRAY)
         
-        _, thresh = cv2.threshold(gray, 0, 250, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        # _, thresh = cv2.threshold(gray, 0, 250, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-        contours, __ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        contours = sorted(contours, key=cv2.contourArea, reverse=True)
+        # contours, __ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # contours = sorted(contours, key=cv2.contourArea, reverse=True)
         
-        second_largest_contour = contours[1]
+        # second_largest_contour = contours[1]
 
-        contour_coords = second_largest_contour[:, 0, :]
-        contour_x_coords = contour_coords[:, 0]
-        contour_y_coords = contour_coords[:, 1]
+        # contour_coords = second_largest_contour[:, 0, :]
+        # contour_x_coords = contour_coords[:, 0]
+        # contour_y_coords = contour_coords[:, 1]
 
-        min_x = min(contour_x_coords) + VISION_BOARD_OFFSET
-        max_x = max(contour_x_coords) - VISION_BOARD_OFFSET
-        min_y = min(contour_y_coords) + VISION_BOARD_OFFSET
-        max_y = max(contour_y_coords) - VISION_BOARD_OFFSET
+        min_x = VISION_CHESSBOARD[0][0]
+        max_x = VISION_CHESSBOARD[0][1]
+        min_y = VISION_CHESSBOARD[1][0]
+        max_y = VISION_CHESSBOARD[1][1]
 
        
         self.chessboard_coords = (min_x, max_x, min_y, max_y)
 
         #Tegner en firkant rundt om brættet
-        #cv2.rectangle(self.visualframe, (int(min_x), int(min_y)), (int(max_x), int(max_y)), (0, 255, 0), 2)
+        # cv2.rectangle(self.oldframe, (int(min_x), int(min_y)), (int(max_x), int(max_y)), (0, 255, 0), 2)
 
     def checkForUpdates(self):
         print("Checking for updates")
         if self.oldframe is not None:
-            #Split the image into 64 squares in a dict 
+            
+            # #Get the size of each square
+            # square_width = (self.chessboard_coords[1] - self.chessboard_coords[0]) / 8
+            # square_height = (self.chessboard_coords[3] - self.chessboard_coords[2]) / 8
 
-            #Get the size of each square
-            square_width = (self.chessboard_coords[1] - self.chessboard_coords[0]) / 8
-            square_height = (self.chessboard_coords[3] - self.chessboard_coords[2]) / 8
-
-            #Loop through each squar
-            _t = False
-            for x in range(0, 8):
-                for y in range(0, 8):
-                    #Get the coordinates of the square
-                    square_x = self.chessboard_coords[0] + (square_width * x)
-                    square_y = self.chessboard_coords[2] + (square_height * y)
+            # #Loop through each squar
+            # _t = False
+            # for x in range(0, 8):
+            #     for y in range(0, 8):
+            #         #Get the coordinates of the square
+            #         square_x = self.chessboard_coords[0] + (square_width * x)
+            #         square_y = self.chessboard_coords[2] + (square_height * y)
 
                 
-                    #Get average color of the square
-                    from_square_x = int(square_x + VISION_SQUARE_OFFSET)
-                    from_square_y = int(square_y + VISION_SQUARE_OFFSET)
-                    to_square_x = int(square_x + square_width - VISION_SQUARE_OFFSET)
-                    to_square_y = int(square_y + square_height - VISION_SQUARE_OFFSET)
+            #         #Get average color of the square
+            #         from_square_x = int(square_x + VISION_SQUARE_OFFSET)
+            #         from_square_y = int(square_y + VISION_SQUARE_OFFSET)
+            #         to_square_x = int(square_x + square_width - VISION_SQUARE_OFFSET)
+            #         to_square_y = int(square_y + square_height - VISION_SQUARE_OFFSET)
 
-                    old_square_color = self.get_square_color(self.oldframe, from_square_x, from_square_y, to_square_x, to_square_y)
-                    new_square_color = self.get_square_color(self.newframe, from_square_x, from_square_y, to_square_x, to_square_y)
+            #         old_square_color = self.get_square_color(self.oldframe, from_square_x, from_square_y, to_square_x, to_square_y)
+            #         new_square_color = self.get_square_color(self.newframe, from_square_x, from_square_y, to_square_x, to_square_y)
 
-                    if not _t:
-                        cv2.imshow("Square1", self.oldframe[from_square_y:to_square_y, from_square_x:to_square_x])
-                        cv2.imshow("Square2", self.newframe[from_square_y:to_square_y, from_square_x:to_square_x])
+            #         if not _t:
+            #             cv2.imshow("Square1", self.oldframe[from_square_y:to_square_y, from_square_x:to_square_x])
+            #             cv2.imshow("Square2", self.newframe[from_square_y:to_square_y, from_square_x:to_square_x])
 
-                        _t = True
+            #             _t = True
 
-                    #Check if the color has changed
-                    print("Square: ", self.index_to_notation(x, y))
-                    print(old_square_color, new_square_color)
+            #         #Check if the color has changed
 
+            #         if self.GetDifference(old_square_color, new_square_color):
+            #             print("Difference found")
+            #             print("Square: ", self.index_to_notation(x, y))
+            #             print(old_square_color, new_square_color)
+                    
+          
+            old_green = cv2.cvtColor(self.oldframe, cv2.COLOR_BGR2HSV)
+            mask = cv2.inRange(old_green, (36, VISION_MASK_VALUE, VISION_MASK_VALUE), (70, 255,255))
+            imask = mask>0
+            old_green = np.zeros_like(self.oldframe, np.uint8)
+            old_green[imask] = self.oldframe[imask]
 
+            new_green = cv2.cvtColor(self.newframe, cv2.COLOR_BGR2HSV)
+            mask = cv2.inRange(new_green, (36, VISION_MASK_VALUE, VISION_MASK_VALUE), (70, 255,255))
+            imask = mask>0
+            new_green = np.zeros_like(self.newframe, np.uint8)
+            new_green[imask] = self.newframe[imask]
 
-            # for x in range(0, self.newframe.shape[0]):
-            #     for y in range(0, self.newframe.shape[1]):
-            #         old_color = self.oldframe[x][y]
-            #         new_color = self.newframe[x][y]
+            cv2.imshow("Old", old_green)
+            cv2.imshow("New", new_green)
+          
+            p_changed = []
+            for x in range(0, new_green.shape[0]):
+                for y in range(0, new_green.shape[1]):
+                    old_color = old_green[x][y]
+                    new_color = new_green[x][y]
 
-            #         is_diff = self.GetDifference(old_color, new_color)
-            #         if is_diff:
-            #             self.oldframe[x][y] = (0, 255, 0)
+                    is_diff = self.GetDifference(old_color, new_color)
+                    if is_diff and self.inside_chessboard(x, y):
+                        self.oldframe[x][y] = (255, 0, 0)
+                        
+    def inside_chessboard(self, x, y):
+        return x > self.chessboard_coords[0] and x < self.chessboard_coords[1] and y > self.chessboard_coords[2] and y < self.chessboard_coords[3]
 
 
     def get_square_color(self, frame, s_x, s_y, s_x0, s_y0):
@@ -114,7 +137,8 @@ class Vision:
         d_g = int(old_color[1]) - int(new_color[1])
         d_b = int(old_color[2]) - int(new_color[2])
 
-        sum = d_r + d_g + d_b
+        
+        sum = abs(d_r) + abs(d_g) + abs(d_b)
         if sum > VISION_DIFF_THRESHOLD:
             return True
         
@@ -123,6 +147,7 @@ class Vision:
     def UpdateOldFrame(self):
         ret, frame = self.cap.read()
         self.oldframe = frame
+        cv2.imwrite('img1.png', frame)
         self.set_chessboard_coords()
 
     def startVision(self):
@@ -130,13 +155,17 @@ class Vision:
             ret, frame = self.cap.read()
             if self.showVideo:
                 cv2.imshow('frame', frame)
+                
            
             if self.oldframe is not None:
                 cv2.imshow('oldframe', self.oldframe)
             
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 self.newframe = frame
+                cv2.imwrite('img2.png', frame)
                 self.checkForUpdates()
+                
+
 
             #If k is pressed, update the old frame
             if cv2.waitKey(1) & 0xFF == ord('k'):
