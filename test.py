2ceac1f8d4a91707c21fef4a028b23602714ce03
Nagodo <89977444+Nagodo@users.noreply.github.com>
2023-04-26 10:46:43 +0200

    vision

--- test.py
+++ test.py
@@ -1,47 +1,84 @@
 import cv2
-
-# Read the image
-image = cv2.imread('test1.jpg')
-
-#Downscale the image
-image = cv2.resize(image, (0,0), fx=0.25, fy=0.25)
-
-# Convert the image to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# Apply thresholding to the image
-_, thresh = cv2.threshold(gray, 0, 220, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+import numpy as np
 
 
-# Find contours in the image
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+img1 = cv2.imread('img1.png')
+img2 = cv2.imread('img2.png')
 
-contours = sorted(contours, key=cv2.contourArea, reverse=True)
-
-second_largest_contour = contours[1]
-
-# Draw the contour on the original image
-cv2.drawContours(image, [second_largest_contour], -1, (0, 255, 0), 3)
-cv2.imshow('Contour', image)
-
-
-contour_coords = second_largest_contour[:, 0, :]
-contour_x_coords = contour_coords[:, 0]
-contour_y_coords = contour_coords[:, 1]
+def find_green_areas(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_green = np.array([50, 50, 50])
+    upper_green = np.array([70, 255, 255])
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    res = cv2.bitwise_and(img1, img1, mask=mask)
+    return res
 
 
-#FInd min and max x and y coords
-min_x = min(contour_x_coords) + 36.5
-max_x = max(contour_x_coords) - 36.5
-min_y = min(contour_y_coords) + 36.5
-max_y = max(contour_y_coords) - 36.5
+green1 = find_green_areas(img1)
+green2 = find_green_areas(img2)
+
+cv2.imshow("a", green1)
 
 
-def index_to_notation(x, y):
-    y = 7 - y
-    return chr(x + 97) + str(y + 1)
+def get_contours(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    contours, __ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
 
 
+def get_valid_contours(contours):
+    valid_contours = []
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > 8 and 100 > area:
+            valid_contours.append(c)
+    return valid_contours
+
+c1 = get_contours(green1)
+c2 = get_contours(green2)
+
+c1 = get_valid_contours(c1)
+c2 = get_valid_contours(c2)
+
+cv2.drawContours(green1, c1, -1, (0, 255, 0), 1)
+cv2.drawContours(green2, c2, -1, (0, 255, 0), 1)
+
+cv2.imshow("a", green1)
+cv2.imshow("b", green2)
+
+c1_coords = []
+c2_coords = []
+for c in c1:
+    coord = cv2.boundingRect(c) 
+    c1_coords.append((coord[0], coord[1]))
+
+for c in c2:
+    coord = cv2.boundingRect(c) 
+    c2_coords.append((coord[0], coord[1]))
+
+
+i = 0
+final_coords = []
+for i in range(len(c1_coords)):
+    found_close = False
+    for j in range(len(c2_coords)):
+        dist_x = abs(c1_coords[i][0] - c2_coords[j][0])
+        dist_y = abs(c1_coords[i][1] - c2_coords[j][1])
+
+        if dist_x < 3.0 and dist_y < 3.0:
+            found_close = True
+            i += 1
+
+            cv2.circle(green1, c1_coords[i-1], 3, (0, 0, 255), -1)
+    
+    if not found_close:
+        final_coords.append(c1_coords[i])
+        cv2.circle(green1, c1_coords[i], 3, (255, 0, 0), -1)
+       
+
+
+cv2.imshow("bdw", green1)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
